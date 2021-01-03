3b1d1e025747c5e048d3c7b85fd06232f0fd8c00
Hugh Hartigan <hartigan.hm@gmail.com>
2021-01-03 14:53:41 -0700

    Initialize initial-docs

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+import { copyContent } from 'shared/data';
+import './LandingPage.scss';
+
+const { heading, subHeading, imageAlt } = copyContent.landingPage;
+
+const LandingPage: FC = () => (
+  <div className="app-initials-landing-page">
+    <h1 className="app-initials-landing-page__heading">{heading}</h1>
+    <p className="app-initials-landing-page__sub-heading">{subHeading}</p>
+    <img
+      className="app-initials-landing-page__image"
+      src={`${process.env.PUBLIC_URL}/assets/logo512.png`}
+      alt={imageAlt}
+    />
+  </div>
+);
+
+export default LandingPage;
