74ab32b97aa8460dba2ae69058477d57ed68c4ca
Hugh Hartigan <hartigan.hm@gmail.com>
2021-01-03 14:53:39 -0700

    Initialize gh-hooks

---
+++ src/shared/data/index.ts
@@ -0,0 +1,8 @@
+import copyContent from './copyContent';
+
+// This is our standard pattern, but should only be used with
+// multiple data objects; Remove the eslint-disable line once another
+// export is added to this file
+
+/* eslint-disable-next-line import/prefer-default-export */
+export { copyContent };
