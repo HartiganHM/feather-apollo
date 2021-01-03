9af429c14973748e2f11799e91615491c77e0afa
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-03 16:44:16 -0700

    chore: Remove comments from data/index

--- src/shared/data/index.ts
+++ src/shared/data/index.ts
@@ -1,9 +1,4 @@
 import copyContent from './copyContent';
 import endpoints from './endpoints';
 
-// This is our standard pattern, but should only be used with
-// multiple data objects; Remove the eslint-disable line once another
-// export is added to this file
-
-/* eslint-disable-next-line import/prefer-default-export */
 export { copyContent, endpoints };
