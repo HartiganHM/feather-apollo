ccc5853ce7648a769323a3b88cf81fdf5788a1b6
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-03 16:40:52 -0700

    chore: Add endpoints data object

--- src/shared/data/index.ts
+++ src/shared/data/index.ts
@@ -1,8 +1,9 @@
 import copyContent from './copyContent';
+import endpoints from './endpoints';
 
 // This is our standard pattern, but should only be used with
 // multiple data objects; Remove the eslint-disable line once another
 // export is added to this file
 
 /* eslint-disable-next-line import/prefer-default-export */
-export { copyContent };
+export { copyContent, endpoints };
