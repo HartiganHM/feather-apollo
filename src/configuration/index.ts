3bcab19e87a3e1d2935a244dad6191c274c2b74a
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-03 19:32:34 -0700

    chore: Add index for /configuration

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,4 +1,8 @@
-// Configurations for things like Apollo, AWS, webpack, etc.
-// should be stored in this directory
+import client from './client';
 
-export default {};
+// This is our standard pattern, but should only be used with
+// multiple mutations; Remove the eslint-disable line once another
+// export is added to this file
+
+/* eslint-disable-next-line import/prefer-default-export */
+export { client };
