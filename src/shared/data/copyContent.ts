5ca55cc5b908614ff255bacea9d173d2c48bae85
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-03 18:42:34 -0700

    chore: Update LandingPage copy

--- src/shared/data/copyContent.ts
+++ src/shared/data/copyContent.ts
@@ -3,5 +3,6 @@ export default {
     heading: 'Feather',
     subHeading: 'by Featherweight Design',
     imageAlt: 'Featherweight Design Logo',
+    getCharacterButton: 'Get Rick and Morty Character',
   },
 };
