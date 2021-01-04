c3a6dce7e7ac5d56d68bd7257614c2bbd109b885
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-04 07:21:44 -0700

    test: Fix order of operations in character tests

--- src/cypress/integration/LandingPage/get-rick-and-morty-character.spec.ts
+++ src/cypress/integration/LandingPage/get-rick-and-morty-character.spec.ts
@@ -3,6 +3,10 @@ import {
   characterCountOperationName,
   charactersByIdAlias,
   charactersByIdOperationName,
+  mockCharacterData,
+  mockCharacterCountResponse,
+  mockCharactersByIdResponse,
+  mockCharacterDescription,
 } from 'cypress/fixtures/get-rick-and-morty-charater.mock';
 import { copyContent, endpoints } from 'shared/data';
 
@@ -10,34 +14,42 @@ const { getCharacterButton } = copyContent.landingPage;
 
 describe('Get Rick and Morty Character', () => {
   before(() => {
-    cy.visit('/');
-  });
-
-  beforeEach(() => {
     cy.intercept(endpoints.graphql, req => {
       const { operationName } = req.body;
       if (operationName === characterCountOperationName) {
         req.alias = characterCountAlias;
+        req.reply(res => {
+          res.body = mockCharacterCountResponse;
+        });
       }
 
       if (operationName === charactersByIdOperationName) {
         req.alias = charactersByIdAlias;
+        req.reply(res => {
+          res.body = mockCharactersByIdResponse;
+        });
       }
     });
+
+    cy.visit('/');
+
+    cy.wait(`@${characterCountAlias}`);
   });
 
   it('Should show a loading circle when the button is clicked', () => {
-    cy.wait(`@${characterCountAlias}`);
     cy.findByText(getCharacterButton).click();
+
+    cy.get('.fd-circle-loader').should('exist');
   });
 
   it("Should show the character's number, name, species, and picture", () => {
-    cy.wait(`@${charactersByIdAlias}`).then(({ response }) => {
-      const { id, name, species } = response?.body?.data?.charactersByIds[0];
-      const expectedString = `${id} | ${name} | ${species}`;
+    cy.findByText(mockCharacterDescription).should('exist');
+  });
 
-      cy.findByText(expectedString).should('exist');
-      cy.findByAltText(name).should('exist');
-    });
+  it('Should show a picture of the character', () => {
+    const characterImage = cy.findByAltText(mockCharacterData.name);
+
+    characterImage.should('exist');
+    characterImage.should('have.attr', 'src', mockCharacterData.image);
   });
 });
