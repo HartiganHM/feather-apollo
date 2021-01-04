eccf234d962992c7617863d93df5c3d41d4a0711
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-04 07:12:58 -0700

    chore: Update mock data for character tests

--- src/cypress/fixtures/get-rick-and-morty-charater.mock.ts
+++ src/cypress/fixtures/get-rick-and-morty-charater.mock.ts
@@ -1,12 +1,41 @@
 const characterCountAlias = 'characterCountQuery';
 const characterCountOperationName = 'GetRickAndMortyCharacterCount';
 
-const charactersByIdAlias = 'charactersByIdQuery'
+const charactersByIdAlias = 'charactersByIdQuery';
 const charactersByIdOperationName = 'GetRickAndMortyCharactersById';
+
+const mockCharacterCountResponse = {
+  data: { characters: { info: { count: 1 } } },
+};
+
+const mockCharacterData = {
+  id: '1',
+  name: 'Rick Sanchez',
+  species: 'Human',
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+};
+
+const mockCharactersByIdResponse = {
+  data: {
+    charactersByIds: [mockCharacterData],
+  },
+};
+
+const {
+  id,
+  name,
+  species,
+  image,
+} = mockCharacterData;
+const mockCharacterDescription = `${id} | ${name} | ${species}`;
 
 export {
   characterCountAlias,
   characterCountOperationName,
   charactersByIdAlias,
   charactersByIdOperationName,
+  mockCharacterData,
+  mockCharacterCountResponse,
+  mockCharactersByIdResponse,
+  mockCharacterDescription,
 };
